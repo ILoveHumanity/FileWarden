d6879d01393f427cd03fe0b06f5abece7af56465
ILoveHumanity <silver-wind@inbox.ru>
2026-02-27 14:02:20 +0700

    test fileinfo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <QCoreApplication>
 
+#include <QString>
+#include <QTextStream>
+#include <QFileInfo>
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
 
-    return a.exec();
+    QString path;
+    QTextStream in(stdin);
+    QTextStream out(stdout);
+    out << "Enter path: " << Qt::flush;
+    path = in.readLine();
+
+    QFileInfo info(path);
+    if(info.isFile()){
+        out << "File exist\t" << path << "\tsize: " << info.size() << Qt::flush;
+    }
+    else{
+        out << "File dont exist\t" << path << Qt::flush;
+    }
+
+
+    while(true){
+        info.refresh();
+        if(info.isFile()){
+            out << "File exist\t" << path << "\tsize: " << info.size() << Qt::flush;
+        }
+        else{
+            out << "File dont exist\t" << path << Qt::flush;
+        }
+    }
+
+    return 0;
+
+    //return a.exec();
 }
