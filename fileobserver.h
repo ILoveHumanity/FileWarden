166e8493365bcb993159a9943559a6776449c9fe
ILoveHumanity <silver-wind@inbox.ru>
2026-03-13 13:34:30 +0700

    Implemented active observation

--- fileobserver.h
+++ fileobserver.h
@@ -1,11 +1,40 @@
-#ifndef FILEOBSERVER_H
-#define FILEOBSERVER_H
+#ifndef FileObserver_H
+#define FileObserver_H
+#pragma once
+
+#include "Ilog.h"
+#include "IObservationSource.h"
+#include <QDateTime>
+#include <QTime>
+#include <QFileInfo>
+#include <QSet>
+#include <QThread>
 
 
 class FileObserver
 {
-public:
+private:
+    QFileInfoList observedFiles;
+    QSet<QString> observedFilesPath;
+    IObservationSource *observationSource;
+    ILog *logger;
+    unsigned int fileStateCheckInterval = 10;
+
+private:
     FileObserver();
+    FileObserver(IObservationSource *observationSource, ILog *logger, unsigned int fileStateCheckInterval);
+    ~FileObserver();
+    FileObserver(const FileObserver &) = delete;
+    FileObserver &operator=(const FileObserver &) = delete;
+
+
+public:
+    static FileObserver& GetInstance(IObservationSource *observationSource, ILog *logger, unsigned int fileStateCheckInterval);
+
+    void setLogger(ILog *logger);
+    void setStateCheckInterval(unsigned int interval);
+
+    void startObservation();
 };
 
-#endif // FILEOBSERVER_H
+#endif // FileObserver_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <QCoreApplication>
-#include <ILog.h>
+#include <fileobserver.h>
+#include <ConsoleLog.h>
+#include <SourceFile.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    ConsoleLog log;
-    QString text = "chto-to";
-    log.Log(text);
+
+    SourceFile file("../source.txt"); // create container for observer
+    ConsoleLog logger; // create logger for observer
+
+    FileObserver &observer = FileObserver::GetInstance(&file, &logger, 5); // put container and logger into observer with refreshRate parameter
+    observer.startObservation(); // run cycle
+
     return app.exec();
 }
