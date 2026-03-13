7bbb7adc275fcb79c056d40cfcd1bc53747e319b
ILoveHumanity <silver-wind@inbox.ru>
2026-03-13 12:52:27 +0700

    Updated file structure for logger

--- ConsoleLog.cpp
+++ ConsoleLog.cpp
@@ -1,5 +1,13 @@
-#include "ILog.h"
+#include "ConsoleLog.h"
 
+ConsoleLog::ConsoleLog()
+{
+
+}
+ConsoleLog::~ConsoleLog()
+{
+
+}
 void ConsoleLog::Log(QString data)
 {
     qDebug() << "Log: " << data;
